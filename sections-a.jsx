5029d42112701f838ba80eff4c3e5412c9e9dc63
Ricardo Albuquerque <rchard.asc@gmail.com>
2026-06-26 10:36:21 -0300

    att cover

--- sections-a.jsx
+++ sections-a.jsx
@@ -129,7 +129,7 @@ function VideoPlaceholder() {
         ref={videoRef}
         className="lp-video__el"
         src={asset('videoChamada', 'assets/chamada-francisco.mp4')}
-        poster={asset('fotoFrancisco', 'assets/foto-francisco.png')}
+        poster="assets/poster-francisco.jpg"
         playsInline
         preload="metadata"
         controls={playing}
